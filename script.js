fd9ca11e5e410d93c8008627c8d685bc2e621765
Sholto <madein.mania@gmail.com>
2021-02-09 09:46:15 +0000

    initial object array

---
+++ script.js
@@ -0,0 +1,22 @@
+//array for library objects
+const library = [
+    {
+        "title": "book1",
+        "author": "author1",
+        "pages": 101,
+        "read": 1,
+    },
+    {
+        "title": "book2",
+        "author": "author2",
+        "pages": 202,
+        "read": 1,
+    },
+    {
+        "title": "book3",
+        "author": "author3",
+        "pages": 303,
+        "read": 1,
+    },
+];
+
